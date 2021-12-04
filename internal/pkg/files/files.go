81928b17487c9744679e4a0329d4cdf122f5f79e
Tim <6232843-cjtim@users.noreply.gitlab.com>
2021-12-04 05:41:09 +0000

    Refactor/project structure

---
+++ internal/pkg/files/files.go
@@ -0,0 +1,87 @@
+package files
+
+import (
+	"context"
+
+	"github.com/cjtim/cjtim-backend-go/internal/app/repository"
+	"github.com/cjtim/cjtim-backend-go/internal/pkg/gstorage"
+	"github.com/cjtim/cjtim-backend-go/internal/pkg/line"
+	"github.com/cjtim/cjtim-backend-go/internal/pkg/rebrandly"
+	"github.com/cjtim/cjtim-backend-go/internal/pkg/utils"
+	"go.mongodb.org/mongo-driver/bson"
+)
+
+// Add - Upload file and save to DB
+func Add(fullFileName string, byteData []byte, lineUID string) (*repository.FileScheama, error) {
+	client, err := gstorage.GetClient()
+	if err != nil {
+		return nil, err
+	}
+	defer client.Close()
+
+	objPath := ("users/" + lineUID + "/files/" + fullFileName)
+	url, err := client.Upload(objPath, byteData)
+	if err != nil {
+		return nil, err
+	}
+	shortURL, err := rebrandly.Add(url)
+	if err != nil {
+		return nil, err
+	}
+	data := &repository.FileScheama{
+		FileName: fullFileName,
+		URL:      *shortURL,
+		LineUID:  lineUID,
+	}
+	_, err = repository.FileRepo.InsertOne(context.TODO(), &data)
+	if err != nil {
+		return nil, err
+	}
+	return data, nil
+}
+
+// AddFromLine - Contents being upload via line chat will parse here
+func AddFromLine(messageID string, lineUID string) (*repository.FileScheama, error) {
+	fileByte, fileType, err := line.GetContent(messageID)
+	if err != nil {
+		return nil, err
+	}
+	ext, err := utils.ContentTypeToExtension(fileType)
+	if ext == nil {
+		ext = []string{""}
+	}
+	if err != nil {
+		return nil, err
+	}
+	return Add(messageID+ext[0], fileByte, lineUID)
+}
+
+// Delete - Remove file from storage and rebrandly
+func Delete(fullFileName string, lineUID string) error {
+	file := repository.FileScheama{}
+	err := repository.FileRepo.FindOne(&file, bson.M{"lineUid": lineUID, "fileName": fullFileName})
+	if err != nil {
+		return err
+	}
+	// Delete from storage
+	gClient, err := gstorage.GetClient()
+	if err != nil {
+		return err
+	}
+	defer gClient.Close()
+
+	err = gClient.Delete("users/" + lineUID + "/files/" + fullFileName)
+	if err != nil {
+		return err
+	}
+	// Delete Rebrandly URL
+	err = rebrandly.Delete(file.URL.RebrandlyID)
+	if err != nil {
+		return err
+	}
+	_, err = repository.FileRepo.DeleteOne(context.TODO(), bson.M{"fileName": fullFileName, "lineUid": lineUID})
+	if err != nil {
+		return err
+	}
+	return nil
+}
---
+++ internal/pkg/line/message_generator.go
@@ -0,0 +1,109 @@
+package line
+
+import (
+	"fmt"
+
+	"github.com/cjtim/cjtim-backend-go/internal/pkg/airvisual"
+)
+
+func WeatherFlexMessage(data *airvisual.AirVisualResponse) map[string]interface{} {
+	var headerMessage, bgColor string
+	AQIValue := fmt.Sprintf("%v", data.Data.Current.Pollution.AQIUS) + " AQIUS"
+	innerMessage := fmt.Sprintf(
+		`City: %v
+AQI us: %v
+AQI cn: %v
+Temperature:%vº
+Humidity: %v
+Wind Speed: %v(m/s)`,
+		data.Data.City,
+		data.Data.Current.Pollution.AQIUS,
+		data.Data.Current.Pollution.AQICN,
+		data.Data.Current.Weather.Temperature,
+		data.Data.Current.Weather.Humidity,
+		data.Data.Current.Weather.WindSpeed,
+	)
+	if data.Data.Current.Pollution.AQIUS <= 50 {
+		headerMessage = "อากาศดีจัง"
+		bgColor = `#27ACB2`
+	} else if data.Data.Current.Pollution.AQIUS > 50 {
+		headerMessage = "อากาศดีพอใช้"
+		bgColor = `#E9AF29`
+	} else if data.Data.Current.Pollution.AQIUS > 100 {
+		headerMessage = "อากาศไม่ค่อยดีนะ"
+		bgColor = `#E9632D`
+	} else {
+		headerMessage = "อากาศแย่มากเลย🤢"
+		bgColor = `#FF6B6E`
+	}
+	return map[string]interface{}{
+		"type":    "flex",
+		"altText": "รายงานอากาศ",
+		"contents": map[string]interface{}{
+			"type": "bubble",
+			"size": "kilo",
+			"header": map[string]interface{}{
+				"type":   "box",
+				"layout": "vertical",
+				"contents": []map[string]interface{}{
+					{
+						"type":    "text",
+						"text":    headerMessage,
+						"color":   "#ffffff",
+						"align":   "start",
+						"size":    "xl",
+						"gravity": "center",
+					},
+					{
+						"type":    "text",
+						"text":    AQIValue,
+						"color":   "#ffffff",
+						"align":   "start",
+						"size":    "xxl",
+						"gravity": "center",
+						"margin":  "lg",
+					},
+				},
+				"backgroundColor": bgColor,
+				"paddingTop":      "19px",
+				"paddingAll":      "12px",
+				"paddingBottom":   "16px",
+				"action": map[string]interface{}{
+					"type":  "uri",
+					"label": "action",
+					"uri":   "https://www.iqair.com/th-en/thailand/bangkok/phaya-thai",
+					"altUri": map[string]interface{}{
+						"desktop": "https://www.iqair.com/th-en/thailand/bangkok/phaya-thai",
+					},
+				},
+			},
+			"body": map[string]interface{}{
+				"type":   "box",
+				"layout": "vertical",
+				"contents": []map[string]interface{}{
+					{
+						"type":   "box",
+						"layout": "horizontal",
+						"contents": []map[string]interface{}{
+							{
+								"type":  "text",
+								"text":  innerMessage,
+								"color": "#000000",
+								"size":  "lg",
+								"wrap":  true,
+							},
+						},
+						"flex": 1,
+					},
+				},
+				"spacing":    "lg",
+				"paddingAll": "12px",
+			},
+			"styles": map[string]interface{}{
+				"footer": map[string]interface{}{
+					"separator": false,
+				},
+			},
+		},
+	}
+}
